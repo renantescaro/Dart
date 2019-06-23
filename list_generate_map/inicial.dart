3ee6925b0a93a0cdb8c7eacb9d48de03f812ea56
renantescaro <renantescaro@yahoo.com.br>
2019-06-23 09:36:13 -0300

    teste

--- list_generate_map/inicial.dart
+++ list_generate_map/inicial.dart
@@ -17,5 +17,38 @@ class Animal{
 }
 
 void main(){
-  
+  var map1 = new Map();
+  var map2 = new Map();
+  var map3 = new Map();
+
+  List<Map> maps = new List<Map>();
+
+  map1['nome'] = 'Vikernes';
+  map1['especie'] = 'Cachorro';
+  map1['idade'] = 5;
+
+  map2['nome'] = 'Sasha';
+  map2['especie'] = 'Gato';
+  map2['idade'] = 2;
+
+  map3['nome'] = 'Frufru';
+  map3['especie'] = 'Galinha';
+  map3['idade'] = 1;
+
+  maps.add(map1);
+  maps.add(map2);
+  maps.add(map3);
+
+  maps.forEach((m) => print(m));
+
+  var listaAnimais = List.generate(maps.length, (i)
+  {
+    return Animal(
+      nome: maps[i]['nome'],
+      especie: maps[i]['especie'],
+      idade: maps[i]['idade'],
+    );
+  });
+
+  listaAnimais.forEach((l) => print(l.Nome));
 }
